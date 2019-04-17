761001af66e45f2f88c517d1863610bbcbe7d5e1
gradi <ionut-adrian.grad@microfocus.com>
2019-04-17 07:20:23 +0300

    added project

--- src/test/java/serviceTests/AddAssignmentTests.java
+++ src/test/java/serviceTests/AddAssignmentTests.java
@@ -20,21 +20,64 @@ public class AddAssignmentTests {
 
     @Test
     public void testAddAssignmentOK() {
-        assertEquals(0, this.service.saveTema("1", "Ionut", 1,3));
-        assertEquals(0, this.service.saveTema("2", "Dragos", 1,3));
+        assertEquals(0, this.service.saveTema("1", "Ionut", 1, 3));
+        assertEquals(0, this.service.saveTema("2", "Dragos", 1, 3));
     }
 
     @Test
-    public void testAddAssignmentIdNotOK() {
+    public void testAddIdNotOK() {
         try {
-            this.service.saveTema(null, "Student1", 1,3);
+            this.service.saveTema(null, "Student1", 1, 3);
         } catch (ValidationException e) {
             Assert.fail();
         }
         try {
-            this.service.saveTema("", "Student1", 1,3);
+            this.service.saveTema("", "Student1", 1, 3);
         } catch (ValidationException e) {
             Assert.fail();
         }
     }
+
+    @Test
+    public void testAddDescriptionNotOK() {
+        try {
+            this.service.saveTema("1", null, 1, 3);
+        } catch (ValidationException e) {
+            Assert.fail();
+        }
+        try {
+            this.service.saveTema("1", "", 1, 3);
+        } catch (ValidationException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testAddDeadlineNotOK() {
+        try {
+            this.service.saveTema("1", "Student1", -1, 3);
+        } catch (ValidationException e) {
+            Assert.fail();
+        }
+        try {
+            this.service.saveTema("", "Student1", 15, 3);
+        } catch (ValidationException e) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testAddStartlineNotOK() {
+        try {
+            this.service.saveTema(null, "Student1", 1, -1);
+        } catch (ValidationException e) {
+            Assert.fail();
+        }
+        try {
+            this.service.saveTema("", "Student1", 1, 14);
+        } catch (ValidationException e) {
+            Assert.fail();
+        }
+    }
+
 }
